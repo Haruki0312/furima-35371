2faf8a859e719533a4874ab25c28ff742450b56b
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-07-22 00:15:40 +0900

    priceカラム記述漏れ修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
   end
 end
