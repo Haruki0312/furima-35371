4387619d28108645ffa7ffc961169b769789ac6f
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-07-21 23:56:19 +0900

    itemモデルのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
-  item.image.attached?
-  valiates :name, presence: true
+  validates :image, presence: true
+  validates :name, presence: true
   validates :info, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :status_id, numericality: { other_than: 1, "can't be blank" }
-  validates :shipping_fee_id, numericality: { other_than: 1, "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, "can't be blank" }
-  validates :schedule_id, numericality: { other_than: 1, "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :schedule_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true
 
   belongs_to :user
