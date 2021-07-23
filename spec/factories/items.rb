32c5bff3ee825233ba66a3553aacd3b6dbed6769
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-07-23 13:07:45 +0900

    itemモデルテストのFactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {'テスト'}
+    info {'テスト説明'}
+    category_id {2}
+    status_id {2}
+    shipping_fee_id {2}
+    prefecture_id {2}
+    schedule_id {2}
+    price {'1000'}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
