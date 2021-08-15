33cbdeb3e87c09c16b6eb8415fde1d5a87932edf
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-08-15 23:01:58 +0900

    Formオブジェクトパターン order_address.rbの作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :tel_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+    validates :city
+    validates :address
+    validates :tel_number, format: { with: /\A\d{11}\z/, message: "can't be blank" }
+    validates :user_id
+    validates :item_id
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, tel_number: tel_number, order_id: order.id)
+  end
+end
