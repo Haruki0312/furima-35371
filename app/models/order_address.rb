183794033fd509b8a0272d4b47904e62555afe12
Haruki0312 <e.k.o.m.0312@icloud.com>
2021-08-18 14:01:21 +0900

    クレジットカード決済機能の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :tel_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :tel_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
@@ -9,6 +9,7 @@ class OrderAddress
     validates :tel_number, format: { with: /\A\d{10,11}\z/ }
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     address {'そこそこ'}
     building {'なになに'}
     tel_number {'09012345678'}
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
